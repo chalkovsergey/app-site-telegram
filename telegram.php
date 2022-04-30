99ab969047cec34be14268ab5f18f0464e333dd1
Sergey <82056215+chalkovsergey@users.noreply.github.com>
2022-04-30 14:16:45 +0500

    Accepting applications from the site in telegram

---
+++ telegram.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "5364042271:AAHbz3kf3aKbvhXPwE-U8b2BiPp2O_gr9iE";
+$chat_id = "-1001614561975";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: success.html');
+} else {
+  echo "Error";
+}
+?>
